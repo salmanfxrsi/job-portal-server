cc63cec7609116292c65aed5deabb52fba8f6cd4
salmanfxrsi <salmanxprivate@gmail.com>
2024-12-14 17:56:01 +0600

    feat: implement MongoDB CRUD operations for job collection

--- index.js
+++ index.js
@@ -8,6 +8,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+// mongodb crud operation
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.upkox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const jobCollection = client.db('jobDB').collection('jobs');
+
+async function run() {
+  try {
+    // await client.connect();
+
+    // get operation
+    app.get('/jobs', async(req,res)=>{
+        const result = await jobCollection.find().toArray();
+        res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req,res) => {
     res.send('server is running...')
 })
